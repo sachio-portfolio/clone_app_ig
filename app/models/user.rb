1f81ba65f14417555c5406b9e351562d401ec9cf
sachio <sachio.engineer@gmail.com>
2021-04-03 19:10:38 +0900

    :sparkles: Add userとpictureのアソシエーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, length: {maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   validates :password, length: { minimum: 6 }
+  has_many :pictures
 end
