99ef391d46ca6d08a7af0e3f04d19d4b52aa53e0
sachio <sachio.engineer@gmail.com>
2021-04-05 22:26:33 +0900

    :+1: Add picturesテーブルのimageカラムにNotNull制約

---
+++ db/migrate/20210405132041_change_pictures_image_not_nill.rb
@@ -0,0 +1,5 @@
+class ChangePicturesImageNotNill < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :pictures, :image, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_100427) do
+ActiveRecord::Schema.define(version: 2021_04_05_132041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pictures", force: :cascade do |t|
-    t.text "image"
+    t.text "image", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
