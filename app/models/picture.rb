96e3c25707fe89c3cd6d61abb1342015d6ef1dca
sachio <sachio.engineer@gmail.com>
2021-04-05 23:40:02 +0900

    :sparkles: Add お気に入り機能作成

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,8 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   validates :image, presence: true
   validates :content, length: { maximum: 2200 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pictures#index'
+  resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :pictures do
