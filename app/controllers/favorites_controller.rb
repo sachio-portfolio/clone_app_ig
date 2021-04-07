d3333c3e676ea561ecfe6f938757f58c67d7f00a
sachio <sachio.engineer@gmail.com>
2021-04-07 15:30:34 +0900

    :sparkles: Add お気に入り一覧追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,7 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = current_user.favorites.all
+  end
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to picture_path(favorite.picture_id)
