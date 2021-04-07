b54151a2d0377bf7350aee2e8ebc40fa0ee14b85
sachio <sachio.engineer@gmail.com>
2021-04-07 10:44:13 +0900

    :+1: Update お気に入り機能修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,13 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
-    redirect_to pictures_path
+    redirect_to picture_path(favorite.picture_id)
   end
   def destroy
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    favorite = current_user.favorites.find_by(id: params[:id])
+    picture_id = favorite.picture_id
+    if favorite.destroy
+      redirect_to picture_path(picture_id)
+    end
   end
 end
